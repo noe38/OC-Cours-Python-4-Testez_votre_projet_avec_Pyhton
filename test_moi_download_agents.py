fe70af038e7b6e124ada94de8c99a58dfcc6a339
noemie graignic <noemie.graignic@free.fr>
2020-02-15 16:02:32 +0100

    utilisez les mocks

---
+++ test_moi_download_agents.py
@@ -0,0 +1,27 @@
+import program.download_agents as script
+
+import urllib.request
+
+from io import BytesIO
+import json
+
+# get_agents()
+
+def test_http_result(tmpdir, monkeypatch):
+    results = [{
+        "age": 84,
+        "agreeablness": 0.74
+    }]
+
+    def mockretrun(request):
+        return BytesIO(json.dumps(results).encode())
+
+    monkeypatch.setattr(urllib.request, 'urlopen', mockretrun)
+
+    p= tmpdir.mkdir('program').join('agents.json')
+
+    script.main(["--dest", str(p), "--count", "1"])
+
+    assert script.get_agents(1) == results
+
+# main()
